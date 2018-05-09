200a81a5093d8dfc64b8c52ad4e77235c9b1c117
root <root@VRLab2.localdomain>
2018-05-09 13:42:38 +0200

    Added options. Fixed audio listener bug

--- Assets/Scripts/UI_Controller.cs
+++ Assets/Scripts/UI_Controller.cs
@@ -8,12 +8,18 @@ public class UI_Controller : MonoBehaviour {
     public InputField textBox;
     [HideInInspector]
     public static GameObject hologram;
+    [HideInInspector]
+    public static GameObject options;
     private TextMesh hologramText;
+    private bool optionsActive;
+    private bool colorStatus = true;    // true = white, false = black
 
 	void Start () {
+        options = GameObject.Find("OptionsMenu");
         hologram = GameObject.Find("holotext");
         hologramText = hologram.GetComponent<TextMesh>();
         hologramText.text = "Hologram is online";
+        options.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -27,4 +33,23 @@ public class UI_Controller : MonoBehaviour {
         hologramText.text = "Changing...";
         hologramText.text = textBox.text;
     } 
+
+    public void OpenOptions()
+    {
+        optionsActive = options.activeSelf;
+        options.SetActive(!optionsActive);
+    }
+
+    public void ChangeColor()
+    {
+        if (colorStatus)
+        {
+            hologramText.color = Color.black;
+            colorStatus = false;
+        } else
+        {
+            hologramText.color = Color.white;
+            colorStatus = true;
+        }
+    }
 }
