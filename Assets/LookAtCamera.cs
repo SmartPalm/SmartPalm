6f84ac43b1e8a6d91435070770c905c6453b3569
root <root@VRLab2.localdomain>
2018-05-15 14:28:49 +0200

    Added new marker database. worked on lookAtCameraScript. Still donst work

--- Assets/LookAtCamera.cs
+++ Assets/LookAtCamera.cs
@@ -3,14 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LookAtCamera : MonoBehaviour {
+    [Tooltip ("Check this box, to stop the shaking of the target object")]
+    public bool lookAtTargetStrictly;
+    [Range(0.0f, 10f)]
+    public float additionOffset;
+    [Range (0.0f, 10f)]
+    public float subtractionOffset;
 
+    private Transform target;
+    private float xRot;
+    private float yRot;
+    private float zRot;
+    private Vector3 newRot;
+    private Transform lateTarget;
+    private float lateYAddOffset;
+    private float lateYSubOffset;
+    
 	// Use this for initialization
 	void Start () {
-		
-	}
+        target = GameObject.Find("ARCamera").transform;
+        lateTarget = target;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        xRot = target.eulerAngles.x;
+        yRot = target.eulerAngles.y;
+        zRot = target.eulerAngles.z;
+        newRot = new Vector3(0, yRot, 0);
+        lateYAddOffset = lateTarget.eulerAngles.y + additionOffset;
+        lateYSubOffset = lateTarget.eulerAngles.y - subtractionOffset;
+
+        if (lookAtTargetStrictly)
+        {
+
+            transform.eulerAngles = newRot;
+            
+        } else
+        {
+            if (yRot > lateYAddOffset || yRot < lateYSubOffset)
+            {
+                transform.eulerAngles = newRot;
+                Debug.Log("Updated");
+            }
+            
+        }
+        lateTarget = target;
+
+        #region TastaturEingabeHilfe
+        if (Input.GetKeyDown("h"))
+        {
+            Debug.Log("X is " + xRot + " Y is " + yRot  + " Z is " + zRot + " and late target is " + (lateTarget.eulerAngles.y + additionOffset));
+            //Debug.Log("The rotation is: " + target.rotation);
+        }
+        if (Input.GetKeyDown("c"))
+        {
+            Debug.Log("Im comparing yRot: " + yRot + " to addOfset: " + lateYAddOffset + " and subOffset: " + lateYSubOffset);
+        }
+        #endregion
+
+    }
 }
